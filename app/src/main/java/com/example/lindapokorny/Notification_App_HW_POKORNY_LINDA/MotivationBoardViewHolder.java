1157897b9d19bffe20bfdfc97cadb711b6a73223
Jose Garcia <jogarcia809@yahoo.com>
2019-02-13 15:09:58 -0500

    completed edits

--- app/src/main/java/com/example/lindapokorny/Notification_App_HW_POKORNY_LINDA/MotivationBoardViewHolder.java
+++ app/src/main/java/com/example/lindapokorny/Notification_App_HW_POKORNY_LINDA/MotivationBoardViewHolder.java
@@ -1,52 +1,39 @@
 package com.example.lindapokorny.Notification_App_HW_POKORNY_LINDA;
 
-import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.example.lindapokorny.motivationboardrecycler.R;
 
 public class MotivationBoardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-    ImageView imageView;
-    TextView textView;
-    int[] images;
-    String[] caption;
-    View.OnClickListener  mclickListener;
-    public MotivationBoardViewHolder(@NonNull View itemView, int[] motivationBoardPictureList, String[] caption) {
-        super(itemView);
-        this.images = images;
-        this.caption = this.caption;
+    private ImageView imageView;
+    private TextView textView;
+    //the listener is the class implementing the interface
+    private MotivationInterface listener;
+    private MotivationBoard board;
 
+    MotivationBoardViewHolder(@NonNull View itemView) {
+        super(itemView);
+        //find your views
         imageView = itemView.findViewById(R.id.motivation_board_picture);
         textView = itemView.findViewById(R.id.info_text);
+        //set the click listener on the itemview
+        itemView.setOnClickListener(this);
+    }
 
-
-        itemView.findViewById(R.id.recyclerview).setOnClickListener(this);
+    void bind(MotivationBoard board, MotivationInterface listener) {
+        this.board = board;
+        this.listener = listener;
+        imageView.setImageResource(board.getPicture());
+        textView.setText(board.getCaption());
     }
 
     @Override
     public void onClick(View view) {
-        if (mclickListener != null) {
-            mclickListener.onClick(imageView);
-        }
-        Intent intent = new Intent(view.getContext(), SecondActivity.class);
-        intent.putExtra("picId", images[getAdapterPosition()]);
-        intent.putExtra("captionId", caption[getAdapterPosition()]);
-        view.getContext().startActivity(intent);
-
-    }
-
-    String getItem(int imageView, int textView){
-        return String.valueOf(imageView) + String.valueOf(textView);
-    }
-    void setClickListener(View.OnClickListener itemClickListener){
-        this.mclickListener = itemClickListener;
-    }
-    public interface ItemClickListener{
-        void onItemClick(View view, int position);
+        //invoke the appropriate method on the listener
+        listener.boardClicked(board);
     }
 }
 
---
+++ app/src/main/java/com/example/lindapokorny/Notification_App_HW_POKORNY_LINDA/MotivationInterface.java
@@ -0,0 +1,6 @@
+package com.example.lindapokorny.Notification_App_HW_POKORNY_LINDA;
+
+// create an interface to define the actions for the corresponding listener to perform
+public interface MotivationInterface {
+    void boardClicked(MotivationBoard board);
+}
